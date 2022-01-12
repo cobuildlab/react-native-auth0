bbd900aa981b8fc492d6dffd0fa25f3535e36e45
kikeztw <kikeztw@gmail.com>
2022-01-12 14:42:16 -0400

    added readme

--- src/hook.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { UseAuthType } from './types';
-import { AuthContext } from './context';
-
-export const useAuth = (): UseAuthType => {
-  const context = React.useContext(AuthContext);
-  return context;
-};
